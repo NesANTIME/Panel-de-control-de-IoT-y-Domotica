abdbe16b3d1d1890a40858f03384d63f3ecac80e
NesANTIME <jacpketmovies@gmail.com>
2026-02-22 16:28:58 -0500

    V0.1

---
+++ controlador_iot_y_domotica/src/main/java/com/controller_iot/controlador_iot_y_domotica/ControladorIotYDomoticaApplication.java
@@ -0,0 +1,13 @@
+package com.controller_iot.controlador_iot_y_domotica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorIotYDomoticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorIotYDomoticaApplication.class, args);
+	}
+
+}
---
+++ controlador_iot_y_domotica/src/test/java/com/controller_iot/controlador_iot_y_domotica/ControladorIotYDomoticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controller_iot.controlador_iot_y_domotica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControladorIotYDomoticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
